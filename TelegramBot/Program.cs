961ed61c7e13757a1861208144d34cd9ca05e2fc
Radik <49549831+aradikdev@users.noreply.github.com>
2023-07-14 10:52:45 +0600

    Update Program.cs

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -13,7 +13,7 @@ namespace DevSpaceConsole
 {
     public class Program
     {
-        private static string _token { get; set; } = "6213536541:AAHPDtBfd9Aa17H4mQt5PL0QpvJl3dBkeBo";
+        private static string _token { get; set; } = "HASH_CODE_FROM_TELEGRAM";
         private static TelegramBotClient _client;
         private static Timer _timer;
         public static void Main()
